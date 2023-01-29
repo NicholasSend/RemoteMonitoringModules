4e671121119d84c7cf7de658bc7f9415167e90d5
bmac1613 <123901572+bmac1613@users.noreply.github.com>
2023-01-29 23:04:06 +0000

    Working test file on arduino
    This file needs to be downloaded as an .ino to be used on arduino.

--- arduino.cpp
+++ arduino.cpp
@@ -1,39 +1,55 @@
 // Arduino file
 
 //includes
-#include <std_msgs/String>
+#include <ros.h>
+#include <stdio.h>
+#include <std_msgs/String.h>
 
 
 //Defines
 #define L_EN 3
 #define R_EN 4
 #define L_PWM 6
-#define L_PWM 5
+#define R_PWM 5
 #define LED 13
 
 ros::NodeHandle nh;
 
 char msg_tmp[4];
+char pre_int[3];
 int PWM_IN;
 
 void messageCb( const std_msgs::String& action){
+  
 	// PWM if else flow
-	msg_tmp = action->data;
-	PWM_IN = std::stoi(msg_tmp.substr(1,3));
+  strcpy(msg_tmp, action.data);
+
+  pre_int[0] = msg_tmp[1];
+  pre_int[1] = msg_tmp[2];
+  pre_int[2] = msg_tmp[3];
+  
+  
+  sscanf(pre_int, "%d", &PWM_IN);
+//	PWM_IN = atoi((msg_tmp[1] + msg_tmp[2] + msg_tmp[3]));
 	
-	if (msg_tmp[0] == "N"){ // Neutral -> no movement
+	if (msg_tmp[0] == 'N'){ // Neutral -> no movement
+  /*
 		digitalWrite(R_PWM, 0);
 		digitalWrite(L_PWM, 0);
+   */
 		digitalWrite(LED, LOW);
 	}
 	
 	else {
+    /*
+	}
 		if (msg_tmp[0] == "F"){	// Forward
+      
 			digitalWrite(R_PWM, PWM_IN);
 			digitalWrite(L_PWM, 0);
 			digitalWrite(LED, HIGH);
 		}
-		
+
 		else if (msg_tmp[0] == "B"){	// Back
 			digitalWrite(R_PWM, 0);
 			digitalWrite(L_PWM, PWM_IN);
@@ -48,8 +64,9 @@ void messageCb( const std_msgs::String& action){
 			digitalWrite(R_PWM, -PWM_IN/2);
 			digitalWrite(L_PWM, PWM_IN/2);
 		}	
+     */
+  digitalWrite(LED, HIGH);   // blink the led
 	}
-	digitalWrite(LED, HIGH);   // blink the led
 }
 
 ros::Subscriber<std_msgs::String> sub("ard_in", &messageCb );
@@ -66,17 +83,19 @@ void setup()
 	pinMode(R_EN, OUTPUT);
 	pinMode(L_PWM, OUTPUT);
 	pinMode(R_PWM, OUTPUT);
+ /*
 	digitalWrite(R_EN, HIGH);
 	digitalWrite(L_EN, HIGH);
+ */
 	nh.initNode();
 	nh.subscribe(sub);
-	nh.advertise(chatter);
+	nh.advertise(pub);
 }
 
 void loop()
 {
 	str_msg.data = hello;
-	chatter.publish( &str_msg );
+	pub.publish( &str_msg );
 	nh.spinOnce();
 	delay(1);
 }
