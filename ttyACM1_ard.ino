9615c26aacf9d4738f8abedacb1decf28590d025
bmac1613 <123901572+bmac1613@users.noreply.github.com>
2023-03-02 01:04:37 +0000

    New Arduino Files & Launch File
    Arduino files have sensor interface and ROS input capabilities, Launch file launched udp_intf, ard_intf, and 2 arduino nodes.

--- ttyACM1_ard.ino
+++ ttyACM1_ard.ino
@@ -7,32 +7,63 @@
 
 
 //Defines
+#define trigPin 10
+#define echoPin 11
+#define irSensorPin A1
 #define L_EN 3
 #define R_EN 4
 #define L_PWM 6
 #define R_PWM 5
 #define LED 13
 
+//distance threshold in cm for Ultrasonic sensor and IR sensor
+#define distanceThresholdUltraSonic 20
+#define distanceThresholdIR 50
+
 ros::NodeHandle nh;
 
 char msg_tmp[4];
 char pre_int[3];
+char flag[1] = '_';
 int PWM_IN;
+long duration;
+int distance;
+int irSensorValue;
+
+//std_msgs::String flag;
 
 void messageCb( const std_msgs::String& action){
   
-  // PWM if else flow
+  // String Manipulation to Integer Values
   strcpy(msg_tmp, action.data);
 
   pre_int[0] = msg_tmp[1];
   pre_int[1] = msg_tmp[2];
   pre_int[2] = msg_tmp[3];
   
-  
   sscanf(pre_int, "%d", &PWM_IN);
-//  PWM_IN = atoi((msg_tmp[1] + msg_tmp[2] + msg_tmp[3]));
+
+  // Collision Avoidance
+  irSensorValue = analogRead(irSensorPin);
+  duration = pulseIn(echoPin, HIGH);
+  distance = duration / 58;
+  // If too close:
+  if ((distance < distanceThresholdUltraSonic) || (irSensorValue < distanceThresholdIR)) {
+    if (flag == '_'){ // if flag is down
+      // Raise flag
+      flag = msg_tmp[0];
+      digitalWrite(LED, LOW);
+      // Raised flag blocks any flagged input from executing and moving further
+    }
+  } 
   
-  if (msg_tmp[0] == 'N'){ // Neutral -> no movement
+  else {
+    // Clear flag
+    flag = '_';
+    digitalWrite(LED, HIGH);
+  }
+  
+  if (msg_tmp[0] == 'N'){ // Neutral -> no movement, safety state
     digitalWrite(R_PWM, 0);
     digitalWrite(L_PWM, 0);
 //   digitalWrite(LED, HIGH); 
@@ -40,31 +71,30 @@ void messageCb( const std_msgs::String& action){
   
   else {
     
-  
-    if (msg_tmp[0] == 'F'){ // Forward
-      
+    if ((msg_tmp[0] == 'F') && (flag != 'F')){ // Forward
       digitalWrite(R_PWM, PWM_IN);
       digitalWrite(L_PWM, 0);
     }
 
-    else if (msg_tmp[0] == 'B'){  // Back
-      digitalWrite(R_PWM, 0);
-      digitalWrite(L_PWM, PWM_IN);
-    }
-
-    
-    else if (msg_tmp[0] == 'R'){  // Right
+    else if ((msg_tmp[0] == 'B') && (flag != 'B')){  // Back
       digitalWrite(R_PWM, 0);
       digitalWrite(L_PWM, PWM_IN);
     }
     
-    else if (msg_tmp[0] == 'L'){  // Left
+    else if ((msg_tmp[0] == 'R') && (flag != 'R')){  // Right
+      digitalWrite(R_PWM, 0);
+      digitalWrite(L_PWM, PWM_IN);
+    }
+    
+    else if ((msg_tmp[0] == 'L') && (flag != 'L')){  // Left
       digitalWrite(R_PWM, PWM_IN);
       digitalWrite(L_PWM, 0);
     } 
-    
-     
-//  digitalWrite(LED, HIGH);   // blink the led
+
+    else{ // Uknown or flagged input -> No movement, safety state
+      digitalWrite(R_PWM, 0);
+      digitalWrite(L_PWM, 0);
+    }
   }
 }
 
@@ -77,6 +107,9 @@ char hello[13] = "hello world!";
 
 void setup()
 {
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(irSensorPin, INPUT);
   pinMode(LED, OUTPUT);
   pinMode(L_EN, OUTPUT);
   pinMode(R_EN, OUTPUT);
@@ -85,6 +118,8 @@ void setup()
 
   digitalWrite(R_EN, HIGH);
   digitalWrite(L_EN, HIGH);
+
+  //Serial.begin(9600);
  
   nh.initNode();
   nh.subscribe(sub);
@@ -93,8 +128,27 @@ void setup()
 
 void loop()
 {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  
+//  duration = pulseIn(echoPin, HIGH);
+//  distance = duration / 58;
+
+  // Ask about serial prints - do we need these piped to the pi?
+//  Serial.print("Distance: ");
+//  Serial.print(distance);
+//  Serial.println(" cm");
+
+//  irSensorValue = analogRead(irSensorPin);
+//  Serial.print("IR Sensor Value: ");
+//  Serial.println(irSensorValue);
+
+
   str_msg.data = hello;
 //  pub.publish( &str_msg );
   nh.spinOnce();
-  delay(1);
+  delay(100);
 }
